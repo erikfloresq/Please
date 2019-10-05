e92eb7d0e28dda96f50e8ef1cca7357fabdf0675
Erik Flores <erikfloresq@gmail.com>
2019-10-04 21:20:45 -0300

    Add json and path utils

--- Sources/Please/Please.swift
+++ Sources/Please/Please.swift
@@ -13,5 +13,22 @@ import UIKit
    Please help you in basic task for your development
 */
 public struct Please {
+
+    public static func readJSON(from file: String) -> Data {
+        guard let jsonFileURL = Bundle.main.url(forResource: file, withExtension: "json") else {
+            fatalError("Problems with JSON URL")
+        }
+        do {
+            return try Data(contentsOf: jsonFileURL)
+        } catch {
+            fatalError("Data parse error: \(error.localizedDescription)")
+        }
+    }
+
+    public static func showSqlLitePath() -> [String] {
+        NSSearchPathForDirectoriesInDomains(.applicationSupportDirectory, .userDomainMask, true)
+        let otherPath = NSSearchPathForDirectoriesInDomains(.applicationSupportDirectory, .userDomainMask, true)
+        return otherPath
+    }
     
 }
